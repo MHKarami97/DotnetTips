fdeeebb613b52b2cff7fbc390a86baba0f96692d
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-04-14 14:07:54 +0430

    change post list view to smart table, add social link to footer, change facicon

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,13 +5,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> and <b><a href="https://mhkarami97.github.io" target="_blank">MHKarami</a></b>
+      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> and <b>
+        <a href="https://mhkarami97.github.io" target="_blank">MHKarami</a></b>
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://github.com/mhkarami97" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://facebook.com/mhkarami97" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://twitter.com/mhkarami97" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://linkedin.com/in/mhkarami97" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
--- src/app/pages/others/post-list/post-list.component.ts
+++ src/app/pages/others/post-list/post-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Setting } from '../../../setting';
-import { PostData } from '../../../models/post/post.module';
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'ngx-post-list',
@@ -12,7 +12,48 @@ export class PostListComponent implements OnInit {
 
   id: string;
   witch: string;
-  lists: PostData[];
+
+  settings = {
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    add: {
+      addButtonContent: '<i class="nb-search"></i>',
+      createButtonContent: '<i class="nb-search"></i>',
+      cancelButtonContent: '<i class="nb-search"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      Id: {
+        title: 'ID',
+        type: 'string',
+        editable: false,
+      },
+      Title: {
+        title: 'عنوان',
+        type: 'string',
+      },
+      CreatedOn: {
+        title: 'تاریخ',
+        type: 'string',
+      },
+    },
+    pager: {
+      perPage: 40,
+    }
+  };
+
+  source: LocalDataSource = new LocalDataSource();
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
@@ -21,18 +62,39 @@ export class PostListComponent implements OnInit {
     this.id = this.route.snapshot.paramMap.get('id');
     this.witch = this.route.snapshot.paramMap.get('witch');
 
-    if (this.witch === '1') {
-      this.lists = Setting.dataList.filter(a => a.User.Id === this.id);
+    if (Setting.dataList) {
+      if (this.witch === '1') {
+        const data = Setting.dataList.filter(a => a.User.Id === this.id);
+        this.source.load(data);
 
-    } else if (this.witch === '2') {
-      this.lists = Setting.dataList.filter(a => a.Tags.Tag.filter(b => b.Id === this.id));
-    } else {
-      this.lists = Setting.dataList;
+      } else if (this.witch === '2') {
+        const data = Setting.dataList.filter(a => a.Tags.Tag.filter(b => b.Id === this.id));
+        this.source.load(data);
+      } else {
+        const data = Setting.dataList;
+        this.source.load(data);
+      }
     }
   }
 
-  onClick(id: string) {
+  onSearch(query: string = '') {
+    if (query === '') {
+      this.source.reset();
+    } else {
+      this.source.setFilter([
+        {
+          field: 'Title',
+          search: query,
+        },
+        {
+          field: 'CreatedOn',
+          search: query,
+        },
+      ], false, true);
+    }
+  }
 
-    this.router.navigate(['/pages/others/single', id]);
+  onClick(value: any) {
+    this.router.navigate(['/pages/others/single', value.data.Id]);
   }
 }
