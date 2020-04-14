f7427f769d0330272c677fe6c503d656c103b183
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-04-14 13:47:11 +0430

    change footer, make pre color better

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> and <b><a href="https://mhkarami97.github.io" target="_blank">MHKarami</a></b>
     </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
